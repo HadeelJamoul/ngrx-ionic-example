0688c8d517cebeef16fcef7f745c4021a3968620
HadeelJamoul <hadeelgamol@gmail.com>
2026-05-17 17:41:49 +0200

    create structure

---
+++ src/app/features/settings/settings.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings.page',
+  templateUrl: './settings.page.html',
+  styleUrls: ['./settings.page.component.scss'],
+})
+export class SettingsPageComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
