9b633ab77a9814fa211961b42404978e72db8b48
HadeelJamoul <hadeelgamol@gmail.com>
2026-05-18 10:19:24 +0200

    add settings state management files including actions, effects, reducer, and initial state

---
+++ src/app/features/settings/data-access/settings.state.ts
@@ -0,0 +1,15 @@
+import { AppTheme } from 'src/app/core/models/app-settings.model';
+
+export interface SettingsState {
+  displayName: string;
+  theme: AppTheme;
+  status: 'idle' | 'loading' | 'error';
+  error: string | null;
+}
+
+export const initialSettingsState: SettingsState = {
+  displayName: '',
+  theme: 'light',
+  status: 'idle',
+  error: null,
+};
